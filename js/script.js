54c07f9a26d747005c9831587c29ef9dfc2ca6ae
Simona Cinti <simona.cinti2@gmail.com>
2020-10-28 17:11:33 +0100

    first toggle menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('hello javascript', $ );
+$(document).ready(function(){
+
+    // Ref
+    var dropLink = $('.with-dropdown > a');
+    var dropMenu = $('.with-dropdown > .dropdown-menu');
+
+    // Show Hide Toggle Menu
+
+    dropLink.click( function (){
+        $(this).next('.dropdown-menu').toggle();
+    });
+
+}); // <--- end doc ready
